f8e24824cd02e689f293c55125c571f0c1437552
Xavimat <xaviermatoses@gmail.com>
2022-05-21 13:08:01 +0200

    Small change in products.js

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,6 @@
 const express = require("express");
-
-const router = express.Router();
-
 const ProductController = require("../controllers/ProductController");
+const router = express.Router();
 
 router.post("/", ProductController.create);
 
