8e544e6e04ab366870fbaa7257d1df4b881d1d0b
Dubesor22 <dubesor22@gmail.com>
2022-05-22 00:47:59 +0200

    endopints categories finished

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -12,6 +12,30 @@ const CategoryController = {
 
       .catch(console.error);
   },
+
+  showAllcategoryProduct(req, res) {
+    Category.findAll().then((category) =>
+      res.status(200).send({ message: "Categories Listed: ", category })
+    );
+  },
+
+  categoryById(req, res) {
+    Category.findOne({
+      where: {
+        id: req.params.id,
+      },
+    }).then((category) =>
+      res.status(200).send({ message: "Categories by Id Listed: ", category })
+    );
+  },
+
+  categorByName(req, res) {
+    Category.findAll({
+      where: { name: req.params.name },
+    }).then((category) =>
+      res.status(200).send({ message: "your Product given By Name:", category })
+    );
+  },
 };
 
 module.exports = CategoryController;
--- routes/categories.js
+++ routes/categories.js
@@ -4,6 +4,15 @@ const router = express.Router();
 
 const CategoryController = require("../controllers/CategoryController");
 
-router.post("/create", CategoryController.create);
+router.post("/", CategoryController.create);
+
+// El endpoint para ver todas las categorías junto a los productos que tienen
+router.get("/", CategoryController.showAllcategoryProduct);
+
+// Crea un endpoint que devuelva una categoría por id
+router.get("/categorybyid/:id", CategoryController.categoryById);
+
+// Filtro para buscar categoría por nombre
+router.get("/categorybyname/:name", CategoryController.categorByName);
 
 module.exports = router;
