19e8a44edcfc3c84e9cdd0ef4892a2a6f195bb43
Xavimat <xaviermatoses@gmail.com>
2022-05-22 19:27:49 +0200

    Better error management
    Added .catch methods and error details included in response

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -3,23 +3,24 @@ const { Category } = require("../models/index.js");
 const CategoryController = {
   create(req, res) {
     Category.create({ ...req.body })
-
       .then((category) =>
         res
           .status(201)
           .send({ message: "Category was successfully created", category })
       )
-
       .catch((err) => {
         console.error(err);
-        res.send({ message: "Some error has occurred" })
+        res.send({ message: "Some error has occurred", err })
       });
   },
 
   showAllcategoryProduct(req, res) {
     Category.findAll().then((category) =>
-      res.status(200).send({ message: "Categories Listed: ", category })
-    );
+      res.status(200).send({ message: "Categories Listed", category })
+    ).catch((err) => {
+      console.error(err);
+      res.send({ message: "Some error has occurred", err })
+    });
   },
 
   categoryById(req, res) {
@@ -29,7 +30,10 @@ const CategoryController = {
       },
     }).then((category) =>
       res.status(200).send({ message: "Categories by Id Listed: ", category })
-    );
+    ).catch((err) => {
+      console.error(err);
+      res.send({ message: "Some error has occurred", err })
+    });
   },
 
   categorByName(req, res) {
@@ -37,7 +41,10 @@ const CategoryController = {
       where: { name: req.params.name },
     }).then((category) =>
       res.status(200).send({ message: "your Product given By Name:", category })
-    );
+    ).catch((err) => {
+      console.error(err);
+      res.send({ message: "Some error has occurred", err })
+    });
   },
 };
 
