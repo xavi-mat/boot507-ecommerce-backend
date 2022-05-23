f1726b7616969892bcd8d59dff825bda183b56aa
Dubesor22 <dubesor22@gmail.com>
2022-05-23 16:55:22 +0200

    reviews create

--- main.js
+++ main.js
@@ -10,5 +10,6 @@ app.use("/products", require("./routes/products"));
 app.use("/categories", require("./routes/categories"));
 app.use("/users", require("./routes/users"));
 app.use("/orders", require("./routes/orders"));
+app.use("/reviews", require("./routes/reviews"));
 
 app.listen(port, () => console.log("Server running in port: " + port));
