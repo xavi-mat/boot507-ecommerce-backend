c16feea1e2ab7ac3f2b4fa92c57b1f3df5ba0d25
Dubesor22 <dubesor22@gmail.com>
2022-05-23 15:53:50 +0200

    token login

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,51 +1,54 @@
-const { User, Token, Sequelize } = require('../models');
+const { User, Token, Sequelize } = require("../models");
 const { Op } = Sequelize;
-const jwt = require('jsonwebtoken');
-const { jwt_secret } = require('../config/config.json')['development']
+const jwt = require("jsonwebtoken");
+const { jwt_secret } = require("../config/config.json")["development"];
 
 const authentication = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization;
-        const payload = jwt.verify(token, jwt_secret);
-        const user = await User.findByPk(payload.id);
-        const tokenFound = await Token.findOne({
-            where: {
-                [Op.and]: [
-                    { UserId: user.id },
-                    { token: token }
-                ]
-            }
-        });
-        if (!tokenFound) {
-            res.status(401).send({ message: 'Unauthorized' });
-        }
-        req.user = user;
-        next();
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({ message: 'Token error', error })
+  try {
+    const token = req.headers.authorization;
+    const payload = jwt.verify(token, jwt_secret);
+    const user = await User.findByPk(payload.id);
+    const tokenFound = await Token.findOne({
+      where: {
+        [Op.and]: [{ UserId: user.id }, { token: token }],
+      },
+    });
+    if (!tokenFound) {
+      res.status(401).send({ message: "Unauthorized" });
     }
-}
-
-const isManager = async (req, res, next) => {
-    if (!['manager'].includes(req.user.role)) {
-        return res.status(403).send({ message: 'Forbidden' });
-    }
+    req.user = user;
     next();
-}
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Token error", error });
+  }
+};
+
+const isAdmin = async (req, res, next) => {
+  if (!["admin"].includes(req.user.role)) {
+    return res.status(403).send({ message: "Forbidden" });
+  }
+  next();
+};
+const isManager = async (req, res, next) => {
+  if (!["manager"].includes(req.user.role)) {
+    return res.status(403).send({ message: "Forbidden" });
+  }
+  next();
+};
 
 const isSeller = async (req, res, next) => {
-    if (!['seller'].includes(req.user.role)) {
-        return res.status(403).send({ message: 'Forbidden' });
-    }
-    next();
-}
+  if (!["seller"].includes(req.user.role)) {
+    return res.status(403).send({ message: "Forbidden" });
+  }
+  next();
+};
 
 const isPremium = async (req, res, next) => {
-    if (!['premium'].includes(req.user.role)) {
-        return res.status(403).send({ message: 'Forbidden' });
-    }
-    next();
-}
+  if (!["premium"].includes(req.user.role)) {
+    return res.status(403).send({ message: "Forbidden" });
+  }
+  next();
+};
 
-module.exports = { authentication, isAdmin, isManager, isSeller, isPremium }
+module.exports = { authentication, isAdmin, isManager, isSeller, isPremium };
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const UserController = require("../controllers/UserController");
 const router = express.Router();
+const { authentication } = require("../middleware/authentication");
 
 router.post("/", UserController.create); // Register a user
 router.post("/login", UserController.login);
-router.delete("/logout", UserController.logout);
-router.get("/", UserController.getUserWithOrders);
+router.delete("/logout", authentication, UserController.logout);
+router.get("/", authentication, UserController.getUserWithOrders);
 
 // TODO: More endpoints
 // router.get("/", UserController.getAll);
