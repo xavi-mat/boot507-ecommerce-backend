5282b20d2b53adc6d46cb31dcf4330367a700736
Xavimat <xaviermatoses@gmail.com>
2022-05-23 13:29:55 +0200

    Endpoint get order

--- routes/orders.js
+++ routes/orders.js
@@ -3,5 +3,7 @@ const OrderController = require("../controllers/OrderController");
 const router = express.Router();
 
 router.post('/', OrderController.create);
+router.post('/addProduct', OrderController.addProduct);
+router.get('/:id', OrderController.getAll);
 
 module.exports = router;
