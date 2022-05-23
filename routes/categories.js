5e3a4c03e152ca1f12b108d63e6194adf5fba142
Xavimat <xaviermatoses@gmail.com>
2022-05-23 16:40:57 +0200

    Delete categories, updated

--- routes/categories.js
+++ routes/categories.js
@@ -5,18 +5,10 @@ const router = express.Router();
 const CategoryController = require("../controllers/CategoryController");
 
 router.post("/", CategoryController.create);
-
-// El endpoint para ver todas las categorías junto a los productos que tienen
 router.get("/", CategoryController.showAllcategoryProduct);
-
-// Crea un endpoint que devuelva una categoría por id
 router.get("/categorybyid/:id", CategoryController.categoryById);
-
-// Filtro para buscar categoría por nombre
 router.get("/categorybyname/:name", CategoryController.categorByName);
-
 router.put("/update/:id", CategoryController.updateCategory);
-
 router.delete("/delete/:id", CategoryController.deleteCategory);
 
 module.exports = router;
