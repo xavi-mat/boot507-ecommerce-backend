387d1b70046629e0171bf91422fe774cbdd531f4
Xavimat <xaviermatoses@gmail.com>
2022-05-23 16:54:12 +0200

    Manager authentication to manage categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,14 +1,13 @@
 const express = require("express");
-
+const CategoryController = require("../controllers/CategoryController");
+const { authentication, isManager } = require("../middleware/authentication");
 const router = express.Router();
 
-const CategoryController = require("../controllers/CategoryController");
-
-router.post("/", CategoryController.create);
+router.post("/", authentication, isManager, CategoryController.create);
 router.get("/", CategoryController.showAllcategoryProduct);
 router.get("/categorybyid/:id", CategoryController.categoryById);
 router.get("/categorybyname/:name", CategoryController.categorByName);
-router.put("/update/:id", CategoryController.updateCategory);
-router.delete("/delete/:id", CategoryController.deleteCategory);
+router.put("/update/:id", authentication, isManager, CategoryController.updateCategory);
+router.delete("/delete/:id", authentication, isManager, CategoryController.deleteCategory);
 
 module.exports = router;
