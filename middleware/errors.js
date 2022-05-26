acc8bf960eb39582273145b3d8790327f61b7ce4
Dubesor22 <dubesor22@gmail.com>
2022-05-26 13:38:33 +0200

    errors fixed

--- middleware/errors.js
+++ middleware/errors.js
@@ -1,29 +1,14 @@
-'use strict';
-
-const handleValidationError = (err, res) => {
-    try {
-        let errors = err.errors.map(el => el.message);
-        const string = errors.join(' || ') + ` (Origin: ${err.origin})`;
-        return res.status(400).send({ messages: string });
-    } catch (error) {
-        return res.status(400).send("ERROR");
-    }
-}
+"use strict";
 
 const typeError = (err, req, res, next) => {
-    const errOrigin = err.origin
-    console.log("typeError", err);
+  console.log("typeError", err);
+  try {
+    let errors = err.errors.map((el) => el.message);
+    const string = errors.join(" || ") + ` (Origin: ${err.origin})`;
+    return res.status(400).send({ messages: string });
+  } catch (error) {
+    return res.status(400).send("ERROR");
+  }
+};
 
-    return handleValidationError(err, res);
-
-    // if(err.name === 'SequelizeValidationError' || err.name === 'SequelizeUniqueConstraintError'){
-    //     return err = handleValidationError(err, res);
-    // } else
-    //     if (errOrigin === 'Post') {
-    //         res.status(500).send('Hubo un problema a la hora de crear un Post');
-    //     } else {
-    //         res.status(500).send('Hubo un problema a la hora de crear un Usuario');
-    //     }
-}
-
-module.exports = { typeError }
+module.exports = { typeError };
