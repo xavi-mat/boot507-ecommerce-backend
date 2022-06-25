b3429706c4c8345264616422c291a8946a673885
Xavimat <xaviermatoses@gmail.com>
2022-06-25 14:23:31 +0200

    Seed: longer description. User Route: /me/

--- routes/users.js
+++ routes/users.js
@@ -11,8 +11,8 @@ router.put("/", authentication, upload.single("avatar"), UserController.updateUs
 router.post("/login", UserController.login);
 router.get("/confirm/:emailToken", UserController.confirm);
 router.delete("/logout", authentication, UserController.logout);
-router.get("/", authentication, UserController.getUserWithOrders);
+router.get("/me", authentication, UserController.getUserWithOrders);
 router.get("/:id", UserController.getPublicUserInfo);
-router.get("/avatar/:avatar", UserController.avatar); 
+router.get("/avatar/:avatar", UserController.avatar);
 
 module.exports = router;
